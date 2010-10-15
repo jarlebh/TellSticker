ff85cb849bd29edc3bddc6ee1efe7315e24b0a4a
Eirik H <eirik.hodne@gmail.com>
2010-10-15 04:08:54 -0700

    Plex/Nine: Update code according to http://forums.plexapp.com/index.php/topic/18324-updating-plugins-to-v2-framework/

--- Contents/Code/tellstick.py
+++ Contents/Code/tellstick.py
@@ -7,11 +7,14 @@
 # ------------------------------------------------------------------------------
 #
 #
-# TellSticker version 0.5 (16th of February, 2010)
+# TellSticker version 0.6 (15th of October, 2010)
 #
 # ------------------------------------------------------------------------------
 # Changelog:
 # ------------------------------------------------------------------------------
+#
+# Version 0.6b (15th of October, 2010)
+# - Try to support Plex/Nine
 #
 # Version 0.6 (19th of February, 2010)
 # - Have to reload the TelldusCore library every time we show the main menu (unfortunately9 to know if there are new or deleted devices
@@ -34,12 +37,6 @@
 # - Initial version
 # - Able to turn devices on or off
 
-
-# PMS plugin framework (only needed if using with Plex Media Server)
-from PMS import *
-from PMS.Objects import *
-from PMS.Shortcuts import *
-
 import ctypes
 from ctypes import *
 from ctypes.util import find_library
@@ -48,7 +45,7 @@ TELLSTICK_TURNON 		= 1
 TELLSTICK_TURNOFF		= 2
 TELLSTICK_BELL			= 4
 TELLSTICK_TOGGLE		= 8
-TELLSTICK_DIM				= 16
+TELLSTICK_DIM			= 16
 
 TELLSTICK_SUCCESS		= 0
 TELLSTICK_ERROR_UNKNOWN = -99
